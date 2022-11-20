5c4ec66b34a4861c4776068294935bba95593d98
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-11-20 20:37:22 -0300

    diferenciando letras certas de erradas

--- src/App.js
+++ src/App.js
@@ -76,7 +76,31 @@ function App() {
 
 // Process the letter input
 const verifyLetter = (letter) => {
-  console.log(letter);
+  
+  const normalizedLetter = letter.toLowerCase()
+
+  if(
+    guessedLetters.includes(normalizedLetter) || 
+    wrongLetters.includes(normalizedLetter)
+  ) {
+    return;
+  }
+
+  if(letters.includes(normalizedLetter)) {
+    setGuessedLetters((actualGuessedLetters) => [
+      ...actualGuessedLetters,
+      normalizedLetter
+    ]);
+  } else {
+    setWrongLetters((actualWrongLetters) => [
+      ...actualWrongLetters,
+      normalizedLetter
+    ]);
+  }
+
+
+    console.log(guessedLetters);
+    console.log(wrongLetters)
 }
 
 // restart the game
