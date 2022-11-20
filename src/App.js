64bada4e8d21ac23438852dc7a9f262fb0a986fe
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-11-20 20:13:06 -0300

    states do jogo

--- src/App.js
+++ src/App.js
@@ -26,6 +26,12 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState ("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
+
+
 
   //escolhendo categoria aleatoria
   const pickWordAndCategory = () => {
@@ -62,7 +68,7 @@ function App() {
     //setando os estado
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
 
     setGameStage(stages[1].name);
@@ -81,7 +87,15 @@ const retry = () => {
   return (
     <div className="App">
      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-     {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+     {gameStage === 'game' && <Game 
+     verifyLetter={verifyLetter} 
+     pickedWord={pickedWord} 
+     pickedCategory={pickedCategory} 
+     letters={letters} 
+     guessedLetters={guessedLetters} 
+     wrongLetters={wrongLetters} 
+     guesses={guesses} 
+     score={score} />}
      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
